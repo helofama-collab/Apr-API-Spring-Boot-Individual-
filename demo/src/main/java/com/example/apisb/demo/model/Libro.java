9eae331f5d3788db63862616a101e303e91419db
Ahmed <ahmed@elmejor.com>
2026-06-15 17:11:18 +0100

    ultimo commit

--- demo/src/main/java/com/example/apisb/demo/model/Libro.java
+++ demo/src/main/java/com/example/apisb/demo/model/Libro.java
@@ -1,11 +1,14 @@
 package com.example.apisb.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Libro {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String autor;
